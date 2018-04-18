977ff30cfdce37d8364a7e3e6f72add671c3cb8f
kylesallette <kylesallette@gmail.com>
2018-04-18 16:47:18 -0600

    adding item controller actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,5 +7,23 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: Item.find(params[:id])
   end
-  
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  def update
+    render json: Item.update(params[:id], item_params)
+  end
+
+  def destroy
+    Item.delete(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+
 end
